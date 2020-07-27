f8ab245c41f8721e9bf2f341ed70f78b0dd8ab2d
Stephen P <stephen@Stephens-MacBook-Pro.local>
2020-07-27 10:16:40 -0400

    Added list, start, and stop EC2 instances

--- shotty/shotty.py
+++ shotty/shotty.py
@@ -1,7 +1,75 @@
+import click
 import boto3
 
+session = boto3.Session(profile_name='default')
+ec2 = session.resource('ec2')
+
+def filter_instances(project):
+    instances = []
+    
+    if project:
+        filters = [{'Name':'tag:Project', 'Values':[project]}]
+        instances = ec2.instances.filter(Filters=filters)
+    else:
+        instances = ec2.instances.all()
+
+    return instances
+
+@click.group()
+def instances():
+    """Commands for instances"""
+
+@instances.command('list')
+@click.option('--project', default=None,help="Only instances for project (tag Project:<name>)")
+def list_instances(project):
+    """List EC2 Instances."""
+    
+    instances = filter_instances(project)
+
+    for i in instances:
+        tags = { t['Key']: t['Value'] for t in i.tags or [] }
+        print(', '.join((
+            i.id,
+            i.instance_type,
+            i.placement['AvailabilityZone'],
+            i.state['Name'],
+            i.public_dns_name,
+            tags.get('Project', '<no project>'))))
+
+    return
+
+
+@instances.command('stop')
+@click.option('--project', default=None,help="Only instances for project (tag Project:<name>)")
+def stop_instances(project):
+    """Stop EC2 instances."""
+    
+    instances = filter_instances(project)
+
+
+    for i in instances:
+        print("Stopping {0}...".format(i.id))
+        i.stop()
+
+@instances.command('start')
+@click.option('--project', default=None,help="Only instances for project (tag Project:<name>)")
+def start_instances(project):
+    """Start EC2 instances."""
+    
+    instances = filter_instances(project)
+
+
+    for i in instances:
+        print("Starting {0}...".format(i.id))
+        i.start()
+
 if __name__ == '__main__':
-    session = boto3.Session(profile_name='default')
-    ec2 = session.resource('ec2')
-    for i in ec2.instances.all():
-        print(i)
+    instances()
+    
+    
+
+
+
+
+
+
